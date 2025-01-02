e613656eac05ff83d77fe5681b6a20cd87a17c08
dima-on <sinica911@gmail.com>
2025-01-02 23:31:42 +0200

    fix

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -5,7 +5,7 @@
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-@set "VIRTUAL_ENV=C:\Users\sinic\Python_Project\Impotant projects\Libs\Quick-doc-py UI\.venv"
+@set "VIRTUAL_ENV=C:\Users\huina\Python Projects\Impotant projects\Libs\Quick-doc-py UI\.venv"
 
 @set "VIRTUAL_ENV_PROMPT=quick-doc-ui-py3.12"
 @if NOT DEFINED VIRTUAL_ENV_PROMPT (
